5c38b0e1f249bb8d7a8113cdbd3f8c3d6a4b5d33
Achim Hilwers <a.hilwers@gs-networks.de>
2019-06-08 14:18:45 +0200

    Code-Formatting

--- app/timeasy/lib/weeklystatistics_widget.dart
+++ app/timeasy/lib/weeklystatistics_widget.dart
@@ -81,13 +81,22 @@ class _WeeklyStatisticsState extends State<WeeklyStatisticsWidget> {
 
   String _getNameOfDay(int weekday) {
     switch (weekday) {
-      case 1 : return "Montag";
-      case 2 : return "Dienstag";
-      case 3 : return "Mittwoch";
-      case 4 : return "Donnerstag";
-      case 5 : return "Freitag";
-      case 6 : return "Samstag";
-      case 7 : return "Sonntag";
+      case 1 :
+        return "Montag";
+      case 2 :
+        return "Dienstag";
+      case 3 :
+        return "Mittwoch";
+      case 4 :
+        return "Donnerstag";
+      case 5 :
+        return "Freitag";
+      case 6 :
+        return "Samstag";
+      case 7 :
+        return "Sonntag";
+      default:
+        return "";
     }
   }
 
