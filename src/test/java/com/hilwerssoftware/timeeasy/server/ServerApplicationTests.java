749d52e10b10b43fec796ee80069ecb87999eee5
Achim Hilwers <a.hilwers@gs-networks.de>
2020-12-12 11:33:43 +0100

    Initial commit of Spring Boot application.

---
+++ src/test/java/com/hilwerssoftware/timeeasy/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilwerssoftware.timeeasy.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
