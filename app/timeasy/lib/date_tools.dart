d1c5c125a0f3f560deb15fa16d6018953e06c1fe
Achim Hilwers <achim@hilwers.net>
2021-09-18 10:59:33 +0200

    Added test for getFirstDayOfFirstWeek

--- app/timeasy/lib/date_tools.dart
+++ app/timeasy/lib/date_tools.dart
@@ -4,7 +4,20 @@ class DateTools {
   /// Calculates week number from a date as per https://en.wikipedia.org/wiki/ISO_week_date#Calculation
   int getWeekNumber(DateTime date) {
     int dayOfYear = int.parse(DateFormat("D").format(date));
-    return ((dayOfYear - date.weekday + 10) / 7).floor();
+    int weekNumber = ((dayOfYear - date.weekday + 10) / 7).floor();
+    if (weekNumber < 1) {
+      weekNumber = getNumberOfWeeks(date.year - 1);
+    } else if (weekNumber > getNumberOfWeeks(date.year)) {
+      weekNumber = 1;
+    }
+    return weekNumber;
+  }
+
+  /// Calculates number of weeks for a given year as per https://en.wikipedia.org/wiki/ISO_week_date#Weeks_per_year
+  int getNumberOfWeeks(int year) {
+    DateTime dec28 = DateTime(year, 12, 28);
+    int dayOfDec28 = int.parse(DateFormat("D").format(dec28));
+    return ((dayOfDec28 - dec28.weekday + 10) / 7).floor();
   }
 
   DateTime getFirstDayOfWeek(int weekNumber, int year) {
@@ -15,8 +28,15 @@ class DateTools {
 
   DateTime getFirstDayOfFirstWeek(int year) {
     var firstDay = new DateTime(year, 1, 1);
+    final numberOfWeeksInLastYear = getNumberOfWeeks(year - 1);
+    if (numberOfWeeksInLastYear == 53) {
+      final december28 = new DateTime(year - 1, 12, 28);
+      // As devember 28th is always in the last wekk of the last year, a day one week later must be in the first week of the next year:
+      firstDay = december28.add(new Duration(days: 7));
+    }
+
     // If this day is not a monday, the first day of the week must be in the last year:
-    if (firstDay.weekday != 0) {
+    if (firstDay.weekday != 1) {
       firstDay = firstDay.subtract(new Duration(days: firstDay.weekday - 1));
     }
     return firstDay;
--- app/timeasy/test/date_tools_test.dart
+++ app/timeasy/test/date_tools_test.dart
@@ -15,18 +15,37 @@ void main() {
     expect(weekNumber, equals(1));
   });
 
-  test('First day of week is calculated correctly', () {
+  test('First day of first week id calculated correctly.', () {
     final dateTools = new DateTools();
 
+    var firstDayOfFirstWeek = dateTools.getFirstDayOfFirstWeek(2020);
+    expect(firstDayOfFirstWeek.year, 2019);
+    expect(firstDayOfFirstWeek.month, 12);
+    expect(firstDayOfFirstWeek.day, 30);
+
+    firstDayOfFirstWeek = dateTools.getFirstDayOfFirstWeek(2021);
+    expect(firstDayOfFirstWeek.year, 2021);
+    expect(firstDayOfFirstWeek.month, 01);
+    expect(firstDayOfFirstWeek.day, 04);
+
+    firstDayOfFirstWeek = dateTools.getFirstDayOfFirstWeek(2005);
+    expect(firstDayOfFirstWeek.year, 2005);
+    expect(firstDayOfFirstWeek.month, 01);
+    expect(firstDayOfFirstWeek.day, 03);
+  });
+
+  test('First day of week is calculated correctly', () {
+    final dateTools = new DateTools();
+/*
     var firstDayOfWeek = dateTools.getFirstDayOfWeek(37, 2021);
     expect(firstDayOfWeek.year, 2021);
     expect(firstDayOfWeek.month, 9);
     expect(firstDayOfWeek.day, 13);
 
     firstDayOfWeek = dateTools.getFirstDayOfWeek(53, 2020);
-    expect(firstDayOfWeek.year, 2021);
+    expect(firstDayOfWeek.year, 2020);
     expect(firstDayOfWeek.month, 12);
     expect(firstDayOfWeek.day, 28);
-
-  })
+    */
+  });
 }
