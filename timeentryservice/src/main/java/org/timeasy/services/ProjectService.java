21ef9848ece517b40794a3828287ae694ea3cfff
Achim Hilwers <achim@hilwers.net>
2022-11-12 00:49:09 +0100

    Projects can be updated now

--- timeentryservice/src/main/java/org/timeasy/services/ProjectService.java
+++ timeentryservice/src/main/java/org/timeasy/services/ProjectService.java
@@ -1,11 +1,16 @@
 package org.timeasy.services;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.LockModeType;
 import javax.transaction.Transactional;
 
 import org.timeasy.models.Project;
 import org.timeasy.repositories.ProjectRepository;
 import org.timeasy.tools.EntityExistsException;
+import org.timeasy.tools.EntityNotFoundException;
 
 @ApplicationScoped
 public class ProjectService {
@@ -22,6 +27,30 @@ public class ProjectService {
             throw new EntityExistsException(String.format("A project with the id %s already exists.", project.getId()));
         }
         projectRepository.persist(project);
+    }
+
+    @Transactional
+    public void update(Project project) throws EntityNotFoundException {
+        doUpdate(project);
+    }
+
+    private void doUpdate(Project project) throws EntityNotFoundException {
+        Project existingProject = projectRepository.findById(project.getId(), LockModeType.PESSIMISTIC_WRITE);
+        if (existingProject == null) {
+            throw new EntityNotFoundException(
+                    String.format("A Project with the id %s could not be found.", project.getId()));
+        }
+        existingProject.setId(project.getId());
+        existingProject.setDescription(project.getDescription());
+        existingProject.setUserId(project.getUserId());
+        existingProject.setDeleted(project.isDeleted());
+        existingProject.setCreatedTimeStamp(project.getCreatedTimeStamp());
+        existingProject.setUpdatedTimeStamp(Instant.now());
+    }
+
+    @Transactional
+    public Project findById(UUID id) {
+        return projectRepository.findById(id);
     }
 
 }
--- timeentryservice/src/test/java/org/timeasy/services/ProjectServiceTest.java
+++ timeentryservice/src/test/java/org/timeasy/services/ProjectServiceTest.java
@@ -1,17 +1,20 @@
 package org.timeasy.services;
 
-import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.List;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.timeasy.models.Project;
 import org.timeasy.repositories.ProjectRepository;
 import org.timeasy.tools.EntityExistsException;
+import org.timeasy.tools.EntityNotFoundException;
 
 import io.quarkus.security.identity.SecurityIdentity;
 import io.quarkus.test.junit.QuarkusTest;
@@ -21,43 +24,82 @@ import io.quarkus.test.junit.mockito.InjectMock;
 @Testcontainers
 public class ProjectServiceTest {
 
-  @InjectMock
-  SecurityIdentity securityIdentity;
-  @Inject
-  private ProjectService projectService;
-  @Inject
-  private ProjectRepository projectRepository;
+    @InjectMock
+    SecurityIdentity securityIdentity;
+    @Inject
+    private ProjectService projectService;
+    @Inject
+    private ProjectRepository projectRepository;
 
-  @BeforeEach
-  @Transactional
-  public void setup() {
-    projectRepository.deleteAll();
-    Mockito.when(securityIdentity.hasRole("user")).thenReturn(true);
-  }
+    @BeforeEach
+    @Transactional
+    public void setup() {
+        projectRepository.deleteAll();
+        Mockito.when(securityIdentity.hasRole("user")).thenReturn(true);
+    }
 
-  @Test
-  public void canProjectBeAdded() throws EntityExistsException {
-    Project project = new Project();
-    project.setDescription("Testproject");
-    projectService.add(project);
+    @Test
+    public void canProjectBeAdded() throws EntityExistsException {
+        Project project = new Project();
+        project.setDescription("Testproject");
+        projectService.add(project);
 
-    Project projectFromDb = projectRepository.findById(project.getId());
-    Assertions.assertEquals(project.getId().toString(), projectFromDb.getId().toString());
-    Assertions.assertEquals(project.getDescription(), projectFromDb.getDescription());
-  }
+        Project projectFromDb = projectRepository.findById(project.getId());
+        Assertions.assertEquals(project.getId().toString(), projectFromDb.getId().toString());
+        Assertions.assertEquals(project.getDescription(), projectFromDb.getDescription());
+    }
 
-  @Test
-  public void addingAProjectFailsIfItAlreadyExists() throws EntityExistsException {
-    Project firstProject = new Project();
-    firstProject.setDescription("First Project");
-    projectService.add(firstProject);
+    @Test
+    public void addingAProjectFailsIfItAlreadyExists() throws EntityExistsException {
+        Project firstProject = new Project();
+        firstProject.setDescription("First Project");
+        projectService.add(firstProject);
 
-    Project secondProject = new Project();
-    secondProject.setId(firstProject.getId());
-    firstProject.setDescription("Second Project");
+        Project secondProject = new Project();
+        secondProject.setId(firstProject.getId());
+        firstProject.setDescription("Second Project");
 
-    Assertions.assertThrows(EntityExistsException.class, () -> {
-      projectService.add(secondProject);
-    });
-  }
+        Assertions.assertThrows(EntityExistsException.class, () -> {
+            projectService.add(secondProject);
+        });
+    }
+
+    @Test
+    public void canProjectBeUpdated() throws EntityExistsException, EntityNotFoundException {
+        Project project = new Project();
+        project.setDescription("Project");
+        project.setUserId("1");
+        project.setDeleted(false);
+        projectService.add(project);
+
+        // Need to re-fetch because the timestamp in the database is not as precise as
+        // the Instant.
+        Instant createdTimeStamp = projectService.findById(project.getId()).getCreatedTimeStamp();
+
+        Project projectToBeUpdated = new Project();
+        projectToBeUpdated.setId(project.getId());
+        projectToBeUpdated.setDescription("Updated description");
+        projectToBeUpdated.setUserId("2");
+        projectToBeUpdated.setDeleted(project.isDeleted());
+        projectToBeUpdated.setCreatedTimeStamp(project.getCreatedTimeStamp());
+        projectToBeUpdated.setUpdatedTimeStamp(project.getUpdatedTimeStamp());
+
+        projectService.update(projectToBeUpdated);
+
+        List<Project> projectList = projectRepository.listAll();
+        Assertions.assertEquals(1, projectList.size());
+        Project updatedProject = projectList.get(0);
+        Assertions.assertEquals(projectToBeUpdated.getDescription(), updatedProject.getDescription());
+        Assertions.assertEquals(projectToBeUpdated.getUserId(), updatedProject.getUserId());
+        Assertions.assertEquals(createdTimeStamp, updatedProject.getCreatedTimeStamp());
+        Assertions.assertNotEquals(project.getUpdatedTimeStamp(), updatedProject.getUpdatedTimeStamp());
+    }
+
+    @Test
+    public void updatingAProjectFailsIfItDoesNotExist() {
+        Project project = new Project();
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            projectService.update(project);
+        });
+    }
 }
