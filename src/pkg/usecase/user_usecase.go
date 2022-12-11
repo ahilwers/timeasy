fa2392ec6f31ded5f477309005cc71f03db74214
Achim Hilwers <achim@hilwers.net>
2022-12-11 17:14:24 +0100

    Hashing password upon adding the user now.

--- src/pkg/usecase/user_usecase.go
+++ src/pkg/usecase/user_usecase.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"timeasy-server/pkg/domain/model"
 	"timeasy-server/pkg/domain/repository"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserUsecase interface {
@@ -28,5 +30,18 @@ func (uu *userUsecase) AddUser(user *model.User) (*model.User, error) {
 	if len(strings.TrimSpace(user.Password)) == 0 {
 		return nil, fmt.Errorf("password must not be empty")
 	}
+	hashedPassword, err := encryptPassword(user.Password)
+	if err != nil {
+		return user, err
+	}
+	user.Password = hashedPassword
 	return uu.userRepo.AddUser(user)
+}
+
+func encryptPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return password, err
+	}
+	return string(hashedPassword), nil
 }
--- src/pkg/usecase/user_usecase_test.go
+++ src/pkg/usecase/user_usecase_test.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"strings"
 	"testing"
 	"timeasy-server/pkg/database"
 	"timeasy-server/pkg/domain/model"
@@ -101,5 +102,39 @@ func Test_userUseCase_AddingUserFailsIfUsernameOnlyContainsSpaces(t *testing.T) 
 		t.Errorf("adding a user without username should cause an error")
 	} else if err.Error() != "username must not be empty" {
 		t.Errorf("wrong error was thrown: %v", err)
+	}
+}
+
+func Test_userUsecase_IsPasswordEncryptedWhenAddingUser(t *testing.T) {
+	teardownTest := test.SetupTest(t)
+	defer teardownTest(t)
+
+	userRepo := database.NewGormUserRepository(test.DB)
+	userUsecase := NewUserUsecase(userRepo)
+	user := model.User{
+		Username: "user",
+		Password: "password",
+	}
+	_, err := userUsecase.AddUser(&user)
+	if err != nil {
+		t.Errorf("error adding the user: %v", err)
+	}
+	users, err := userRepo.GetAllUsers()
+	if err != nil {
+		t.Errorf("error getting users from database: %v", err)
+	}
+	if len(users) == 0 {
+		t.Error("user dataset was not created")
+	}
+	userFromDb := users[0]
+	if userFromDb.Username != user.Username {
+		t.Errorf("username wrong - expected: %v, actual: %v", user.Username, userFromDb.Username)
+	}
+	// Password should not be the same as the one from the created user.
+	if userFromDb.Password == "password" {
+		t.Errorf("the password doesn't seem to be encrypted: %v", userFromDb.Password)
+	}
+	if len(strings.TrimSpace(userFromDb.Password)) == 0 {
+		t.Error("the password of the saved user should not be empty")
 	}
 }
