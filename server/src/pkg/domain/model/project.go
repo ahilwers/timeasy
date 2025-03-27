bcc2b663e00c09899e1ffddd3a42028ccb3b7524
Achim Hilwers <achim@hilwers.net>
2025-03-27 06:31:22 +0100

    Added default value for color

--- server/src/pkg/domain/model/project.go
+++ server/src/pkg/domain/model/project.go
@@ -14,7 +14,7 @@ type Project struct {
 	UserId            uuid.UUID  `gorm:"type:uuid;" json:"userId"`
 	TeamID            *uuid.UUID `gorm:"type:uuid;" json:"teamId"` // Team is optional
 	Team              Team
-	Color             string          `json:"color"`
+	Color             string          `gorm:"default:#1E90FF;" json:"color"`
 	Deadline          time.Time       `gorm:"type:date;" json:"deadline"`
 	HourlyRate        decimal.Decimal `gorm:"type:numeric(10,2);default:0.00;" json:"hourlyRate"`
 	TimeBudgetInHours int             `gorm:"default:0;" json:"budget"`
