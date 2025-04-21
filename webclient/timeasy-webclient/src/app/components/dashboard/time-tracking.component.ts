18281ba09b156418ac1882927a9728e2c6d9de99
Achim Hilwers <achim@hilwers.net>
2025-04-21 09:55:19 +0200

    Time tracking component is working now. #73

--- webclient/timeasy-webclient/src/app/components/dashboard/time-tracking.component.ts
+++ webclient/timeasy-webclient/src/app/components/dashboard/time-tracking.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, effect, inject, OnInit, signal, ViewChild} from '@angular/core';
 import {FloatLabel} from 'primeng/floatlabel';
 import {InputText} from 'primeng/inputtext';
 import {Button} from 'primeng/button';
@@ -6,6 +6,9 @@ import {Select} from 'primeng/select';
 import {TranslatePipe} from '@ngx-translate/core';
 import {ProjectService} from '../../services/project.service';
 import {TimerComponent} from './timer.component';
+import {TimeEntryService} from '../../services/time-entry.service';
+import {FormsModule} from '@angular/forms';
+import {TimeEntry} from '../../models/timeentry.model';
 
 @Component({
   selector: 'app-time-tracking',
@@ -18,25 +21,113 @@ import {TimerComponent} from './timer.component';
     Button,
     Select,
     TranslatePipe,
-    TimerComponent
+    TimerComponent,
+    FormsModule
   ]
 })
 
 export class TimeTrackingComponent implements OnInit {
   private readonly projectService = inject(ProjectService);
+  private readonly timeEntryService = inject(TimeEntryService);
+  private debounceTimer: any;
+
+  @ViewChild('timerRef') timerComponent!: TimerComponent;
 
   projects = this.projectService.projects();
+  project = this.projectService.project();
+  lastOpenTimeEntry = this.timeEntryService.lastOpenTimeEntry();
+  addedTimeEntry = this.timeEntryService.lastUpdatedTimeEntry();
+
   selectedProject: any = null;
+  timeEntry: TimeEntry | null = null;
+  description = signal<string>('')
+  descriptionManuallyChanged = signal<boolean>(false)
+  running: boolean = false;
+
+  constructor() {
+    effect(() => {
+      if (this.lastOpenTimeEntry()) {
+        this.timeEntry = this.lastOpenTimeEntry();
+        if (this.timeEntry) {
+          this.descriptionManuallyChanged.set(false);
+          this.description.set(this.timeEntry.description);
+          this.projectService.loadProject(this.timeEntry.projectId);
+          this.timerComponent?.start(new Date(this.timeEntry.startTime));
+        }
+        this.running = true;
+      }
+    });
+    effect(() => {
+      if (this.project()) {
+        console.log("project loaded")
+        this.selectedProject = this.project();
+      }
+    });
+    effect(() => {
+      if (this.addedTimeEntry()) {
+        this.timeEntryService.loadLastOpenTimeEntry()
+      }
+    });
+    effect(() => {
+      if (this.descriptionManuallyChanged()) {
+        const value = this.description();
+        console.log("tt");
+        clearTimeout(this.debounceTimer);
+        this.debounceTimer = setTimeout(() => {
+          this.saveDescription(value);
+        }, 500);
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.projectService.loadProjects();
+    this.timeEntryService.loadLastOpenTimeEntry();
   }
 
   onPlay() {
+    if (this.selectedProject) {
+      const timeEntry: TimeEntry = {
+        id: '',
+        projectId: this.selectedProject.id,
+        startTime: new Date(),
+        endTime: undefined,
+        description: this.description()
+      }
+      this.timeEntryService.addTimeEntry(timeEntry)
+      this.running = true;
+      this.timerComponent?.start();
+    }
+  }
 
+  onPause() {
+    if (this.timeEntry) {
+      this.timeEntry.endTime = new Date();
+      this.timeEntryService.updateTimeEntry(this.timeEntry);
+    }
+    this.running = false;
+    this.timerComponent.stop();
+    this.timerComponent.reset();
   }
 
   projectSelected(event: any) {
     this.selectedProject = event.value;
+    if (this.running && this.timeEntry) {
+      this.timeEntry.projectId = this.selectedProject.id;
+      this.timeEntryService.updateTimeEntry(this.timeEntry);
+    }
+  }
+
+  saveDescription(description: string) {
+    if (this.timeEntry) {
+      console.log("saving description")
+      this.timeEntry.description = description;
+      this.timeEntryService.updateTimeEntry(this.timeEntry);
+    }
+  }
+
+  onDescriptionChanged(newValue: string) {
+    this.description.set(newValue);
+    this.descriptionManuallyChanged.set(true);
   }
 }
